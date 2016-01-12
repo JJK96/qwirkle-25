7b2883dea9fddcbb0752cbe4436e478da9c44423
Sam <Sam@MSI-SAM>
2016-01-12 10:23:50 +0100

    Aanpassingen in game en player

--- server/Game.java
+++ server/Game.java
@@ -1,5 +1,6 @@
 package server;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
@@ -8,15 +9,14 @@ public class Game {
 	private Player[] players;
 	private Player currentPlayer;
 
-	public Game() {
-
+	public Game(String[] names) {
+		players = new Player[names.length];
+		for (int i=0; i<names.length; i++) {
+			players[i] = new Player(names[i], this);
+		}
 	}
 
 	public void start(){
-		
-	}
-
-	public void play(){
 		
 	}
 
@@ -26,6 +26,19 @@ public class Game {
 
 	public List<Stone> stones(){
 		return bag;
+	}
+	
+	/* returns the amount of stones to the player
+	 * @param: player and amount
+	 */
+	public void giveStones(Player player, int amount) {
+		List<Stone> randomStones = new ArrayList<Stone>();
+		for (int i = 0; i < amount; i++) {
+			int place = ((int) Math.random() * (bag.size() + 1));
+			randomStones.add(bag.get(place));
+			bag.remove(place);
+		}
+		player.takeStones(randomStones);
 	}
 
 	public Player currentPlayer(){
