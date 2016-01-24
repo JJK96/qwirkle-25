cf97362f0e89a1b677d9f4a6349d03049bba50ce
Sam <Sam@MSI-SAM.fritz.box>
2016-01-24 18:18:43 +0100

    Javadoc meh wat janjaap heeft gemaakt snap ik vaak niet goed genoeg om te javadoccen :(

--- shared/Space.java
+++ shared/Space.java
@@ -13,39 +13,76 @@ public class Space {
 	private List<Stone> column;
 	private List<Stone> row;
 
+	/**
+	 * creates a space.
+	 */
 	public Space() {
 		this.column = new ArrayList<Stone>();
 		this.row = new ArrayList<Stone>();
 	}
 
+	/**
+	 * Get the column of a space.
+	 * 
+	 * @return List of stones in that column
+	 */
 	public List<Stone> getColumn() {
 		return column;
 	}
 
+	/**
+	 * Sets the column of a list of stones.
+	 * 
+	 * @param column
+	 */
 	public void setColumn(List<Stone> column) {
 		this.column = column;
 	}
 
+	/**
+	 * Adds a column to every stone of the specified list.
+	 * 
+	 * @param stoneList
+	 */
 	public void addColumn(List<Stone> stoneList) {
 		for (Stone s : stoneList) {
 			addColumn(s);
 		}
 	}
 
+	/**
+	 * Adds a column to the stone.
+	 * 
+	 * @param s
+	 */
 	public void addColumn(Stone s) {
 		this.column.add(s);
 	}
 
+	/**
+	 * Adds a row to every stone in the specified list.
+	 * 
+	 * @param stoneList
+	 */
 	public void addRow(List<Stone> stoneList) {
 		for (Stone s : stoneList) {
 			addRow(s);
 		}
 	}
 
+	/**
+	 * Adds a row to the stone.
+	 * 
+	 * @param s
+	 */
 	public void addRow(Stone s) {
 		this.row.add(s);
 	}
 
+	/**
+	 * Sets the row of the //////////////////////////////////////////////////////////////////////////
+	 * @param row
+	 */
 	public void setRow(List<Stone> row) {
 		this.row = row;
 	}
@@ -58,7 +95,7 @@ public class Space {
 		return position;
 	}
 
-	//@ requires position.length == 2;
+	// @ requires position.length == 2;
 	public void setPosition(Position position) {
 		this.position = position;
 	}
